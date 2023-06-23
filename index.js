6720374f2090306e6b88b5c4277fc6839c0dec5b
Slaasiri <136016184+Slaasiri@users.noreply.github.com>
2023-06-23 08:17:00 -0500

    assignment
    6/23

--- index.js
+++ index.js
@@ -1,45 +1,181 @@
-// VARIABLES
-// let, var, const
+// // // // let, var, const
 
-//console.log(name); // print to the console
-const name = "pizza";
-const topping = "pineapple";
-/* adding two strings; concatenation */
-console.log(name, topping);
+// // // //console.log(name); // print to the console
+// // // //const name = "pizza";
+// // // //const topping = "pineapple";
+// // // /* adding two strings; concatenation */
+// // // //console.log(name, topping);
 
-// TEMPLATE LITERALS
-console.log(`${name} ${topping}`);
+// // // // TEMPLATE LITERALS
+// // // //console.log(`${name} ${topping}`);
 
-console.log("32" + "100" + "string");
+// // // //console.log("32" + "100" + "string");
 
-// integer
+// // // // integer
 
-// booleans
+// // // // booleans
 
-// false, undefined, null, 0
+// // // // false, undefined, null, 0
 
-const num = undefined;
+// // // const num = undefined;
 
-console.log(Boolean(num));
+// // // console.log(Boolean(num));
 
-//console.log(Boolean(topping));
+// // // //console.log(Boolean(topping));
 
-//let prompt1 = prompt("what is your name?");
-//let whereDidHeGo = prompt("where did you go ");
-//console.log(prompt1 + "str");
-//console.log(whereDidHeGo);
-//console.log(`${prompt1}went to the ${whereDidHeGo}`);
+// // // //let prompt1 = prompt("what is your name?");
+// // // //let whereDidHeGo = prompt("where did you go ");
+// // // //console.log(prompt1 + "str");
+// // // //console.log(whereDidHeGo);
+// // // //console.log(`${prompt1}went to the ${whereDidHeGo}`);
 
-//alert(`${prompt1}went to the ${whereDidHeGo}`);
+// // // //alert(`${prompt1}went to the ${whereDidHeGo}`);
 
-//name age hobby alert personalied introduction message
+// // // //name age hobby alert personalied introduction message
 
-//hi my name is i am age i like to hobby
+// // // //hi my name is i am age i like to hobby
 
-let prompt1 = prompt("what is your name?");
-let howOldAreYou = prompt("How old are you ");
-let whatIsYourHobby = prompt("What is one of your hobbies? ");
+// // // let prompt1 = prompt("what is your name?");
+// // // let howOldAreYou = prompt("How old are you ");
+// // // let whatIsYourHobby = prompt("What is one of your hobbies? ");
 
-alert(
-  `hi my name is ${prompt1} my age is ${howOldAreYou} and i like to practise  ${whatIsYourHobby}`
-);
+// // // alert(
+// // //   `hi my name is ${prompt1} my age is ${howOldAreYou} and I like to practise ${whatIsYourHobby}`
+// // // );
+
+// // // //let name = prompt("what is your name");
+
+// // // //let age = prompt("how old are you");
+
+// // // //let hobby = prompt("what is one of your hobbies");
+
+// // // //alert(`hi my name is ${name} my age is ${age} one of my hobbies is ${hobbies}`);
+
+// // // //DATA TYPE AND VARIABLES
+// // // // blah blah blah
+// // // CONDITIONALS////////////////
+
+// // // truthy values
+
+// // //falsy values
+// // // undefined, null , Nan, 0
+
+// // //conditional operators
+// // // ===;    1===1 is true
+// // //==; loose equals;
+
+// // // <, >,
+// // //if statement
+
+// // // if (this is true ) {then run this}
+
+// // // if ("collin" === "collin") {
+// // //   console.log("hello world");
+// // // }
+
+// // //logical operator
+// // // ||  && ??
+// // // ||
+// // //
+
+// // // let x = 0;
+// // // let y = 1;
+
+// // // if (x === 0 && y === 1) {
+// // //   console.log("hello world");
+// // // }
+
+// // let i = 0;
+
+// // let k = 1;
+
+// // if (i === 1 || k === 1) {
+// //   console.log("hello world");
+// // }
+
+// // // if this happen or this
+
+// // // ELSE; if the statment is
+
+// // let v = 10;
+// // let u = 8;
+// // let login = false;
+
+// // if (v === 10) {
+// //   console.log("hello world");
+// // } else {
+// //   console.log("goodbye world");
+// // }
+
+// // if (login === true) {
+// //   //redirect to page
+// // } else {
+// //   alert("user doesn't exist");
+// // }
+
+// // else if
+// /* will run if conditions before it are false and specifies a
+// new condition */
+// let q = 0;
+// if (q === 1) {
+//   console.log("goodbye world");
+// } else if (q === 0) {
+//   console.log("hello world");
+// }
+
+// let user = "guest";
+
+// if (user === "admin") {
+//   //redirect to admin page
+// } else if (user === "user") {
+// } else {
+//   alert("account not found");
+// }
+
+// let username = prompt("ENTER your name ");
+
+// if (username === "collin123" || "leyanna325") {
+//   alert("logged in");
+// } else {
+//   alert("No account found");
+// }
+// let randomnumber = Math.floor(Math.random() * 4);
+// console.log(randomnumber);
+
+// let arr = ["liv", "kamar", " derrick"];
+
+// console.log(arr[randomnumber]);
+
+// if (arr[randomnumber] === "liv") {
+//    console.log("hello world");
+// } else if (arr[randomnumber] === ('kamar')
+//   {
+//  console.log("hello")
+// }
+// else {
+// //   console.log("tech your wrong");
+// // }
+
+// let ar = ["shadi", "lucy", "kite"];
+// if (ar[0] === "shadi") {
+//   console.log("shadi");
+// } else if (ar[1] === "lucy") {
+//   console.log("lucy");
+// }
+
+// let randomnumber = Math.random;
+// {
+//   console.log(randomnuber);
+// }
+
+let x = Math.floor(Math.random() * 11);
+let y = prompt("Guess a number from 0 to 10.");
+if (x === y);
+{
+
+  alert("you are correct");
+  if (x === y);
+}
+{
+  alert(`you are incorrest the answer is ${randomNumber}`);
+}
